e25fa0523829fd63a0d303d9512daad80dcf7291
lxdiyun <lxdiyun@gmail.com>
2013-03-24 18:27:14 +0800

    Add item location user models and admin

---
+++ admin.py
@@ -0,0 +1,76 @@
+from models import User, Location, Item, Log
+from django.contrib import admin
+from daterange_filter.filter import DateRangeFilter
+from datetime import datetime
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = ['id', 'name']
+
+
+class LocationAdmin(admin.ModelAdmin):
+    model = Location
+    list_display = ['id', 'name']
+
+
+class LogInline(admin.TabularInline):
+    model = Log
+    readonly_fields = ['operator', 'date']
+
+    def save_model(self, request, obj, form, change):
+        print("new log")
+        obj.operator = request.user
+        super(LogInline, self).save(request, obj, form, change)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name',
+                    'sn',
+                    'sn2',
+                    'buy_date',
+                    'last_modify_date',
+                    'last_modify_by',
+                    'user',
+                    'location',
+                    'comments']
+    search_fields = ['name', 'comments', 'user', 'location', 'sn', 'sn2']
+    list_filter = [('last_modify_date', DateRangeFilter),
+                   'last_modify_by',
+                   'user__name',
+                   'location__name',
+                   ('buy_date', DateRangeFilter),
+                   ]
+    inlines = [LogInline]
+    exclude = ['last_modify_by']
+
+    def save_model(self, request, obj, form, change):
+        obj.last_modify_by = request.user
+        obj.last_modify_date = datetime.now()
+        obj.save()
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+
+        for instance in instances:
+            # Check if it is the correct type of inline
+            if isinstance(instance, Log):
+                if not instance.operator_id:
+                    instance.operator = request.user
+
+                instance.save()
+
+    def construct_change_message(self, request, form, formsets):
+        message = super(ItemAdmin, self).construct_change_message(request,
+                                                                  form,
+                                                                  formsets)
+        print(form.cleaned_data)
+        for item in form.changed_data:
+            message += "\n%s => %s" % (item, form.cleaned_data.get(item))
+
+        return message
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Item, ItemAdmin)
--- models.py
+++ models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User as Operator
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=250, verbose_name=_('item user'))
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=250, verbose_name=_('location'))
+
+    class Meta:
+        verbose_name = _('location')
+        verbose_name_plural = _('locations')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Log(models.Model):
+    date = models.DateField(auto_now_add=True,
+                            verbose_name=_('date'))
+    description = models.TextField(blank=True,
+                                   null=True,
+                                   verbose_name=_("description"))
+    operator = models.ForeignKey(Operator, verbose_name=_('operator'))
+    item = models.ForeignKey('Item')
+
+    class Meta:
+        verbose_name = _('log')
+        verbose_name_plural = _('logs')
+
+    def __unicode__(self):
+        return self.description
+
+
+class Item(models.Model):
+    STATUS = (
+        (0, _('store up')),
+        (1, _('in use')),
+        (2, _('scrap')),
+    )
+    name = models.CharField(max_length=250, verbose_name=_('item name'))
+    sn = models.CharField(max_length=128,
+                          null=True,
+                          blank=True,
+                          verbose_name=_('sn'))
+    sn2 = models.CharField(max_length=128,
+                           null=True,
+                           blank=True,
+                           verbose_name=_('sn/2'))
+    status = models.IntegerField(max_length=8,
+                                 choices=STATUS,
+                                 default=0,
+                                 verbose_name=_('status'))
+    comments = models.TextField(blank=True,
+                                null=True,
+                                verbose_name=_('comments'))
+    buy_date = models.DateField(null=True,
+                                blank=True,
+                                verbose_name=_('buy date'))
+    last_modify_date = models.DateField(auto_now_add=True,
+                                        verbose_name=_('last modify date'))
+    last_modify_by = models.ForeignKey(Operator,
+                                       verbose_name=_('last modify by'))
+    user = models.ForeignKey(User)
+    location = models.ForeignKey(Location)
+
+    class Meta:
+        ordering = ['-last_modify_date']
+        verbose_name = _('item')
+        verbose_name_plural = _('items')
+
+    def __unicode__(self):
+        return self.name
