2ca058f16be814c50d7f914f5512db4e42bd55e3
lxdiyun <lxdiyun@gmail.com>
2013-03-31 21:02:37 +0800

    add clong admin action

--- admin.py
+++ admin.py
@@ -2,27 +2,27 @@ from models import *
 from django.contrib import admin
 from daterange_filter.filter import DateRangeFilter
 from datetime import datetime
-from adli.admin_actions import export_csv_action
+from adli.admin_actions import export_csv_action, clone_action
 
 
 class ItemTypeAdmin(admin.ModelAdmin):
     model = ItemType
-    list_display = ['id', 'name']
+    list_display = ['name']
 
 
 class ManufacturerAdmin(admin.ModelAdmin):
     model = Manufacturer
-    list_display = ['id', 'name']
+    list_display = ['name']
 
 
 class UserAdmin(admin.ModelAdmin):
     model = User
-    list_display = ['id', 'name']
+    list_display = ['name']
 
 
 class LocationAdmin(admin.ModelAdmin):
     model = Location
-    list_display = ['id', 'name']
+    list_display = ['name']
 
 
 class LogInline(admin.TabularInline):
@@ -30,13 +30,13 @@ class LogInline(admin.TabularInline):
     readonly_fields = ['operator', 'time']
 
     def save_model(self, request, obj, form, change):
-        print("new log")
         obj.operator = request.user
         super(LogInline, self).save(request, obj, form, change)
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = [f.name for f in Item._meta.fields] + ['get_recent_logs']
+    list_display = ([f.name for f in Item._meta.fields]
+                    + ['get_recent_logs'])
     search_fields = ['name', 'comments', 'user', 'location', 'sn', 'sn2']
     list_filter = [('last_modify_date', DateRangeFilter),
                    'last_modify_by',
@@ -46,7 +46,9 @@ class ItemAdmin(admin.ModelAdmin):
                    ]
     inlines = [LogInline]
     exclude = ['last_modify_by']
-    actions = [export_csv_action(extra=['get_recent_logs'])]
+    actions = [export_csv_action(extra=['get_recent_logs'],
+                                 exclude=['id']),
+               clone_action()]
 
     def save_model(self, request, obj, form, change):
         obj.last_modify_by = request.user
@@ -73,6 +75,18 @@ class ItemAdmin(admin.ModelAdmin):
             message += "\n%s => %s" % (item, form.cleaned_data.get(item))
 
         return message
+
+    def clone(self, obj, request):
+            new_kwargs = dict()
+            exclude = ['id', 'last_modify_by', 'last_modify_date']
+            for fld in self.model._meta.fields:
+                if fld.name not in exclude:
+                    new_kwargs[fld.name] = getattr(obj, fld.name)
+
+            new_kwargs['last_modify_by'] = request.user
+            new_kwargs['last_modify_date'] = datetime.now()
+
+            self.model.objects.create(**new_kwargs)
 
 
 admin.site.register(Item, ItemAdmin)
