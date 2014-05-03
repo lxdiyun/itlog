59b03e9e4a0f925ec3de81ff1c594db996bf174f
lxdiyun <lxdiyun@gmail.com>
2014-05-03 18:00:16 +0800

    add ordering and serch api

--- views.py
+++ views.py
@@ -1,9 +1,14 @@
 # Create your views here.
 
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from models import Resource
 
 
 class ResourceViewSet(viewsets.ReadOnlyModelViewSet):
     model = Resource
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+    filter_fields = ['name', 'sn', 'sn2', 'number', 'record_date']
+    ordering_fields = '__all__'
+    ordering = ['-record_date']
+    search_fields = ['name', 'model', 'specification', 'user', 'sn', 'sn2']
