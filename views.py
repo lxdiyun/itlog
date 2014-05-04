a8ab692aef33463192ccef490e733d6b63787f78
Adrain Lee <lxdiyun@gmail.com>
2014-05-04 11:31:32 +0800

    add api search field

--- views.py
+++ views.py
@@ -8,7 +8,9 @@ from models import Resource
 class ResourceViewSet(viewsets.ReadOnlyModelViewSet):
     model = Resource
     filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
-    filter_fields = ['name', 'sn', 'sn2', 'number', 'record_date']
+    filter_fields = ['name', 'sn', 'sn2', 'catalog_id', 'national_id',
+                     'number', 'record_date']
     ordering_fields = '__all__'
     ordering = ['-record_date']
-    search_fields = ['name', 'model', 'specification', 'user', 'sn', 'sn2']
+    search_fields = ['name', 'model', 'specification', 'catalog_id',
+                     'national_id', 'user', 'sn', 'sn2']
